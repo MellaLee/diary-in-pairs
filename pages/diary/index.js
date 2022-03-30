ce6d16a4ed42146fc4b5ee068ab9b557d2ec5bd3
Mella <1033707163@qq.com>
2022-03-30 09:49:55 +0800

    init:初始化日记项目

---
+++ pages/diary/index.js
@@ -0,0 +1,14 @@
+Page({
+    data: {
+      diary: []
+    },
+    onLoad() {
+      this.setData({
+        diary: [{
+            title: '十一表演吃空气',
+            content: '没有什么才艺，就给大家表演一个吃空气吧~'
+        }]
+      })
+    }
+  })
+  
